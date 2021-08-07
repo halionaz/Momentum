446eeec0a140c058ee36d3fa9e6349a1c4e74272
haSaJa <samhsj@naver.com>
2021-08-07 19:24:10 +0900

    Update Clock

--- js/system.js
+++ js/system.js
@@ -1,7 +1,14 @@
 const body = document.querySelector('body');
-const img = document.querySelector('img');
+const date = document.querySelector('#jsDate');
+const clock = document.querySelector('#jsClock');
 
 body.style.backgroundImage = `url(./img/${Math.floor(Math.random()*4)}.jpg)`;
-// img.src = `./img/${Math.floor(Math.random()*4)}.jpg`;
-// console.log();
-// body.style.backgroundImage = `../img/${Math.floor(Math.random()*4)}.jpg`
+
+function clockDisplay(){
+    const now = new Date();
+    date.innerHTML = `${now.getFullYear()}.${String(now.getMonth()+1).padStart(2,'0')}.${String(now.getDate()).padStart(2,'0')}`;
+    clock.innerHTML = `${String(now.getHours()).padStart(2,'0')}:${String(now.getMinutes()).padStart(2,'0')}:${String(now.getSeconds()).padStart(2,'0')}`
+}
+
+clockDisplay();
+setInterval(clockDisplay,1000);
