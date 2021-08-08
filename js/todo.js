c923f987d5a1bbb2266506a877c827d33ec1a98e
haSaJa <samhsj@naver.com>
2021-08-08 16:08:52 +0900

    TodoList Add

---
+++ js/todo.js
@@ -0,0 +1,57 @@
+
+const todoList = document.querySelector(".todolist");
+const todoForm = document.querySelector(".todoForm");
+const todoInput = document.querySelector(".todoInput");
+const addBtn = document.querySelector("#jsAddIcon");
+
+let list = [];
+let id;
+
+function add(todo,ind, done){
+    done = done ? `radio-button-on-outline`: `radio-button-off-outline`;
+    const item = document.createElement('li');
+    item.className='todoList_item';
+    const doneIcon = document.createElement('ion-icon');
+    doneIcon.name = done;
+    const content = document.createElement('div');
+    content.className = 'listText';
+    content.innerHTML = todo;
+    item.appendChild(doneIcon);
+    item.appendChild(content);
+    console.log(item);
+    todoList.appendChild(item);
+}
+
+function inputAdd(event){
+    event.preventDefault();
+    if(todoInput.value && id < 13){
+        add(todoInput.value, id, false);
+        list.push({todo:todoInput.value,ind:id,done:false});
+        localStorage.setItem('TODO',JSON.stringify(list));
+        id++;
+    }
+    todoInput.value = '';
+}
+
+function displayList(arr){
+    arr.forEach((item)=>{
+        add(item.todo,item.ind,item.done);
+    });
+}
+
+function getData(){
+    const data = localStorage.getItem('TODO');
+    if(data == null){
+        id = 0;
+        list = [];
+    } else {
+        list = JSON.parse(data);
+        id = list.length;
+        displayList(list);
+    }
+}
+
+getData();
+
+todoForm.addEventListener("submit",inputAdd);
+addBtn.addEventListener("click",inputAdd);
