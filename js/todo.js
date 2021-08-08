109a544d817fd2572e6562af69bd97d7bcc44bee
haSaJa <samhsj@naver.com>
2021-08-08 16:31:12 +0900

    todolist making complete

--- js/todo.js
+++ js/todo.js
@@ -11,20 +11,23 @@ function add(todo,ind, done){
     done = done ? `radio-button-on-outline`: `radio-button-off-outline`;
     const item = document.createElement('li');
     item.className='todoList_item';
+    item.id=ind;
     const doneIcon = document.createElement('ion-icon');
     doneIcon.name = done;
+    doneIcon.id = ind;
+    doneIcon.addEventListener('click',doneIt);
     const content = document.createElement('div');
     content.className = 'listText';
     content.innerHTML = todo;
     item.appendChild(doneIcon);
     item.appendChild(content);
-    console.log(item);
     todoList.appendChild(item);
+    console.log(id);
 }
 
 function inputAdd(event){
     event.preventDefault();
-    if(todoInput.value && id < 13){
+    if(todoInput.value && list.length < 13){
         add(todoInput.value, id, false);
         list.push({todo:todoInput.value,ind:id,done:false});
         localStorage.setItem('TODO',JSON.stringify(list));
@@ -46,9 +49,26 @@ function getData(){
         list = [];
     } else {
         list = JSON.parse(data);
-        id = list.length;
-        displayList(list);
+        if(list.length == 0){
+            id = 0;
+        } else {
+            id = (list[list.length-1].ind)+1;
+        }
     }
+    displayList(list);
+}
+
+function doneIt(event){
+    const deleteId = event.target.id;
+    for(let i = 0; i < list.length; i++){
+        if(list[i].ind === parseInt(deleteId)){
+            console.log(list[i]);
+            list.splice(i,1);
+        }
+    }
+    todoList.innerHTML = '';
+    localStorage.setItem('TODO',JSON.stringify(list));
+    displayList(list);
 }
 
 getData();
